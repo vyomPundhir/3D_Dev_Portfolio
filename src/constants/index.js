6d77955f181c97084c7cadbb262f25182f2f3704
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-18 23:39:01 +0530

    Constants folder added.

---
+++ src/constants/index.js
@@ -0,0 +1,121 @@
+export const categoryNames = [
+  {
+    id: 'htmlcss',
+    title: 'HTML & CSS'
+  },
+  {
+    id: 'js',
+    title: 'JavaScript'
+  },
+  {
+    id: 'rjs',
+    title: 'React'
+  }
+];
+
+// export const technologies = [
+// 	{
+// 		name: "HTML 5",
+// 		icon: html,
+// 	},
+// 	{
+// 		name: "CSS 3",
+// 		icon: css,
+// 	},
+// 	{
+// 		name: "Tailwind CSS",
+// 		icon: tailwind,
+// 	},
+// 	{
+// 		name: "JavaScript",
+// 		icon: javascript,
+// 	},
+// 	// {
+// 	// 	name: "TypeScript",
+// 	// 	icon: typescript,
+// 	// },
+// 	{
+// 		name: "React JS",
+// 		icon: reactjs,
+// 	},
+// 	// {
+// 	// 	name: "Redux Toolkit",
+// 	// 	icon: redux,
+// 	// },
+// 	{
+// 		name: "Zustand",
+// 		icon: zustand,
+// 	},
+
+// 	// {
+// 	// 	name: "MongoDB",
+// 	// 	icon: mongodb,
+// 	// },
+// 	// {
+// 	// 	name: "Express JS",
+// 	// 	icon: express,
+// 	// },
+// 	// {
+// 	// 	name: "Node JS",
+// 	// 	icon: nodejs,
+// 	// },
+// 	// {
+// 	// 	name: "Three JS",
+// 	// 	icon: threejs,
+// 	// },
+
+// 	{
+// 		name: "git",
+// 		icon: git,
+// 	},
+// 	// {
+// 	// 	name: "figma",
+// 	// 	icon: figma,
+// 	// },
+// 	// {
+// 	// 	name: "docker",
+// 	// 	icon: docker,
+// 	// },
+// ];
+
+// // export const blogs = [
+// // 	{
+// // 		name: "JavaScript Array & It's Methods",
+// // 		image: arrayimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/javascript-array-and-its-methods",
+// // 	},
+// // 	{
+// // 		name: "REST & SPREAD Operators in JavaScript",
+// // 		image: restimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/rest-spread-operators-in-javascript",
+// // 	},
+// // 	{
+// // 		name: "Getting Started with ExpressJS",
+// // 		image: expressimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/getting-started-with-expressjs",
+// // 	},
+// // ];
+
+// export const htmlCssProjects = [
+//   {
+// 		name: "Product HomePage",
+// 		description:
+// 			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+// 		tags: [
+// 			{
+// 				name: "html",
+// 				color: "green-text-gradient",
+// 			},
+// 			{
+// 				name: "css",
+// 				color: "pink-text-gradient",
+// 			},
+// 		],
+// 		image: product,
+// 		source_code_link: "https://github.com/yashPundhir/Product_HomePage",
+// 		live_link: "https://product-homepage-sj.netlify.app/",
+// 	},
+// ]
