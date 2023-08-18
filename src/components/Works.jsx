0b8277a77a51735d9405d3deeff23626a3cadba9
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-18 23:34:02 +0530

    works jsx file modified.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { categoryNames } from '../constants';
 import WebDesign from './WebDesign'
 import Development from './Development'
 import ProductDesign  from "./ProductDesign";
 import { useState } from 'react';
+import Card from './Card';
 
 const Works = () => {
 
@@ -10,25 +12,37 @@ const Works = () => {
 
   return (
     <div className='m-0 p-0 box-border h-screen text-white snap-center'>
-      <section className='flex justify-center'>
-        <div className="left basis-1/2 flex ml-[-400px]">
-          <div className="flex flex-col gap-[20px]">
-            <div className='item text-[80px] font-[700] cursor-pointer text-transparent relative' onClick={()=>setWork("Web Design")}>Web Design</div>
+      <section className='flex flex-col justify-center items-center'>
+        <div className="flex justify-center items-center gap-[100px]">
+
+            {/* <div className='item px-5 text-[80px] font-[700] cursor-pointer text-transparent relative hover:shadow-md' onClick={()=>setWork("HTML & CSS")}>HTML & CSS</div>
+
             <div className='item text-[80px] font-[700] cursor-pointer text-transparent'
-            onClick={()=>setWork("Development")}>Development</div>
-            <div className='item text-[80px] font-[700] cursor-pointer text-transparent'>Illustration</div>
-            <div className='item text-[80px] font-[700] cursor-pointer text-transparent'  onClick={()=>setWork("Product Design")}>Product Design</div>
-            <div className='item text-[80px] font-[700] cursor-pointer text-transparent'>Social Media</div>
-          </div>
+            onClick={()=>setWork("JavaScript")}>JavaScript</div>
+
+            <div className='item text-[80px] font-[700] cursor-pointer text-transparent'  onClick={()=>setWork("React")}>ReactJS</div> */}
+
+            {
+              categoryNames.map((item)=>(
+                <button key={item.id} className='w-[450px] item py-3 text-[50px] text-center font-[700] cursor-pointer text-transparent relative hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] focus:shadow-[0px_0px_30px_5px_rgba(0,255,89)] rounded-2xl'>
+                  {item.title}
+                </button>
+              ))
+            }
+
         </div>
         
 
         <div className="right-works">
-          {work === "Web Design" ? (<WebDesign/>) : work === "Development" ? (<Development/>) : (<ProductDesign />)}
+          {work === "HTML & CSS" ? (<WebDesign/>) : work === "JavaScript" ? (<Development/>) : (<ProductDesign/>)}
         </div>
       </section>
+      <div>
+        <Card/>
+      </div>
     </div>
   )
 }
 
 export default Works
+// hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)]
