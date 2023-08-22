d622669bab667d0037703d07e8ac45181b023ae7
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-23 00:26:56 +0530

    Card jsx file added.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt';
 
 const Card = () => {
   return (
-<div className="card w-96 bg-base-100 shadow-xl">
+  <Tilt>
+    <div className="card w-96 bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
     <div>
       <img src="https://dummyimage.com/720x400" alt="Shoes" className="rounded-xl" />
     </div>
-    <div>
-      <img src="src\assets\clip.svg" alt="clip" className='w-[80px]'/>
-      <img src="src\assets\github.svg" alt="github" className='w-[150px]'/>
-    </div>
+    
 
   </figure>
+
+    <div className='flex gap-2 absolute z-10 mt-5 left-3/4'>
+      <img src="src\assets\github.svg" alt="github" className='w-[40px] border-2 rounded-full'/>
+      {/* <img src="src\assets\clip.svg" alt="clip" className='w-[40px]'/> */}
+      <img src="src\assets\paper-clip.svg" alt="clip" className='w-[40px] border-2 rounded-full bg-white'/>
+    </div>
+
   <div className="card-body items-center text-center">
     <h2 className="card-title">Title</h2>
     <p>Description</p>
@@ -22,7 +28,8 @@ const Card = () => {
       <span className="btn btn-primary">tech</span>
     </div>
   </div>
-</div>  )
+</div>
+  </Tilt>  )
 }
 
 export default Card
