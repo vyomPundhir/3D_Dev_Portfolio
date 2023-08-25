4251335b9aae063ae15a8ab042fc0cef8d5c22cd
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-25 22:09:22 +0530

    constants indexjs file updated.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,26 @@
+import seo from '../assets/seomaster.png'
+import crypto from '../assets/cryptomarket.png'
+import fashion from '../assets/fashionhub.png'
+import calm from '../assets/calm.png'
+import medifine from '../assets/medifine.png'
+import justice from '../assets/justice.png'
+import creditcard from '../assets/creditcard.png'
+import gaming from '../assets/gaming.png'
+import realestate from '../assets/realestate.png'
+import beats from '../assets/beats.png'
+import dataanalytics from '../assets/dataanalytics.png'
+import vscode from '../assets/vscode.png'
+
+
 export const categoryNames = [
   {
     id: 'htmlcss',
     title: 'HTML & CSS'
   },
+	{
+		id: 'tailwindcss',
+    title: 'Tailwind CSS'
+	},
   {
     id: 'js',
     title: 'JavaScript'
@@ -12,6 +30,242 @@ export const categoryNames = [
     title: 'React'
   }
 ];
+
+export const htmlCssProjects = [
+  {
+		name: "SEO Master HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: seo,
+		source_code_link: "https://github.com/vyomPundhir/SEO_Master_Homepage",
+		live_link: "https://seo-master-homepage.vercel.app/",
+	},
+  {
+		name: "Crypto Market HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: crypto,
+		source_code_link: "https://github.com/vyomPundhir/Crypto_Market_Homepage",
+		live_link: "https://crypto-market-homepage.vercel.app/",
+	},
+  {
+		name: "Fashion Hub HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: fashion,
+		source_code_link: "https://github.com/vyomPundhir/Fashion_Hub_Homepage",
+		live_link: "https://fashion-hub-homepage.vercel.app/",
+	},
+  {
+		name: "Calm HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: calm,
+		source_code_link: "https://github.com/vyomPundhir/Calm_Homepage",
+		live_link: "https://calm-homepage.vercel.app/",
+	},
+  {
+		name: "Medifine HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: medifine,
+		source_code_link: "https://github.com/vyomPundhir/Medifine_Homepage",
+		live_link: "https://medifine-homepage.vercel.app/",
+	},
+  {
+		name: "Justice HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: justice,
+		source_code_link: "https://github.com/vyomPundhir/Justice_Homepage",
+		live_link: "https://justice-homepage.vercel.app/",
+	},
+  {
+		name: "Credit Card Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: creditcard,
+		source_code_link: "https://github.com/vyomPundhir/Credit_Card_Landing_Page",
+		live_link: "https://credit-card-landing-page.vercel.app/",
+	},
+  {
+		name: "Gaming Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: gaming,
+		source_code_link: "https://github.com/vyomPundhir/Gaming-Landing-Page",
+		live_link: "https://gaming-landing-page-six.vercel.app/",
+	},
+  {
+		name: "Real Estate Desktop Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: realestate,
+		source_code_link: "https://github.com/vyomPundhir/Real-Estate-Desktop-Page",
+		live_link: "https://real-estate-desktop-page.vercel.app/",
+	},
+  {
+		name: "Beats Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: beats,
+		source_code_link: "https://github.com/vyomPundhir/Beats-Landing-Page",
+		live_link: "https://beats-landing-page-alpha.vercel.app/",
+	},
+  {
+		name: "Data Analytics Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: dataanalytics,
+		source_code_link: "https://github.com/vyomPundhir/Data-Analytics-Landing-Page",
+		live_link: "https://data-analytics-landing-page.vercel.app/",
+	},
+]
+
+export const tailwindProjects = [
+	{
+		name: "VScode HomePage UI Clone",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+			{
+				name: "tailwindcss",
+				color: "#00f6ff",
+			},
+		],
+		image: vscode,
+		source_code_link: "https://github.com/vyomPundhir/VScode_Homepage_UI_Clone",
+		live_link: "https://vscode-homepage-ui-clone.vercel.app/",
+	},
+]
+
+export const javascriptProjects =[
+
+]
+
+export const reactjsProjects = [
+
+]
+
+
 
 // export const technologies = [
 // 	{
@@ -98,24 +352,3 @@ export const categoryNames = [
 // // 			"https://yashpundhir.hashnode.dev/getting-started-with-expressjs",
 // // 	},
 // // ];
-
-// export const htmlCssProjects = [
-//   {
-// 		name: "Product HomePage",
-// 		description:
-// 			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
-// 		tags: [
-// 			{
-// 				name: "html",
-// 				color: "green-text-gradient",
-// 			},
-// 			{
-// 				name: "css",
-// 				color: "pink-text-gradient",
-// 			},
-// 		],
-// 		image: product,
-// 		source_code_link: "https://github.com/yashPundhir/Product_HomePage",
-// 		live_link: "https://product-homepage-sj.netlify.app/",
-// 	},
-// ]
