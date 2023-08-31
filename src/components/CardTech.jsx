8f1cab1b25bc0b00ea5aa0d9e792f5cdb245efb7
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-31 23:40:22 +0530

    CardTech jsx file added.

---
+++ src/components/CardTech.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CardTech = () => {
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src='' alt="tech" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">HTML5</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CardTech
