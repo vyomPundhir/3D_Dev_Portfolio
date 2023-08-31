e94173bfd5785cd821f03a2881f8dcdc9a08453a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-08-31 23:41:00 +0530

    TechStack jsx file added.

---
+++ src/components/TechStack.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CardTech from './CardTech'
+
+
+const TechStack = () => {
+  return (
+    <div>
+      <h1 className='text-[white]'>Tech Stack</h1>
+      <div>
+        <CardTech/>
+      </div>
+    </div>
+  )
+}
+
+export default TechStack
