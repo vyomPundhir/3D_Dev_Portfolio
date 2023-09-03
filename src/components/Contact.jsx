37a2401054f17cb87ec4036ec31ea2f04135cce6
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:13:56 +0530

    Conatct jsx file modified.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
 };
 
   return (
-    <div className='m-0 p-0 box-border snap-center w-full h-screen bg-black'>
+    <div id='contactus' className='m-0 p-0 box-border snap-center w-full h-screen bg-black'>
       <section className='w-full h-full flex justify-center gap-[120px]'>
 
         <div className="left basis-1/2 flex justify-end items-center">
@@ -30,13 +30,13 @@ const Contact = () => {
           <form ref={ref} className="form w-[500px] flex flex-col gap-[25px] justify-center items-start" onSubmit={handleSubmit}>
             <div className="title font-[700] text-[74px]">Contact Us</div>
 
-            <input placeholder='Name' className='w-full p-[20px] bg-transparent border-[3px] border-[white] rounded-[5px] text-[white] font-[700]' name="name"/>
+            <input placeholder='Name' className='w-full p-[20px] bg-transparent border-[3px] border-[white] rounded-[10px] text-[white] font-[700]' name="name"/>
 
-            <input placeholder='Email' className='w-full p-[20px] bg-transparent border-[3px] border-[white] rounded-[5px] text-[white] font-[700]' name="email"/>
+            <input placeholder='Email' className='w-full p-[20px] bg-transparent border-[3px] border-[white] rounded-[10px] text-[white] font-[700]' name="email"/>
 
-            <textarea placeholder='Write your message' className='w-full p-[20px] bg-transparent border-[3px] border-[white] rounded-[5px] text-[white] font-[700]' name="message"/>
+            <textarea placeholder='Write your message' className='w-full h-[180px] p-[20px] bg-transparent border-[3px] border-[white] rounded-[10px] text-[white] font-[700]' name="message"/>
 
-            <button className='w-full bg-transparent text-[#1eff00] text-[22px] font-[700] border-[3px] border-[#1eff00] cursor-pointer rounded-[5px] p-[20px] hover:shadow-[0px_0px_30px_5px_#1eff00] hover:bg-[#c1c1c1] hover:text-[#0c5b00] transition ease-in-out duration-500' type='submit'>Send</button>
+            <button className='w-full bg-transparent text-[#1eff00] text-[22px] font-[700] border-[3px] border-[#1eff00] cursor-pointer rounded-[10px] p-[20px] hover:shadow-[0px_0px_30px_5px_#1eff00]  hover:text-[#1eff00] transition ease-in-out duration-500' type='submit'>Send</button>
             {success &&
               "Your Message has been sent. We will get back to you soon..."
             }
@@ -44,9 +44,6 @@ const Contact = () => {
         </div>
 
         <div className="right basis-1/2 flex justify-center items-center">
-          {/* <video loop autoplay>
-            <source src={earth} type="video/mp4" />
-          </video> */}
           <img src="src\assets\earth.gif" alt="earth" className='rounded-full' />
         </div>
       </section>
