172f277af7c10860c20d2c869e97cb7ee1b95e9e
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:17:03 +0530

    Who jsx file modified.

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 
 const Who = () => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    <div className='p-0 box-border h-auto text-white snap-center flex justify-center'>
+    <div id='about' className='p-0 box-border h-auto text-white snap-center flex justify-center'>
       <section className='text-[white] min-h-screen snap-center w-[1400px]
       flex justify-center gap-[190px]'>
 
         <div className='left-who flex-1 flex justify-center items-center'>
-          <img src="src\assets\laser.gif" alt="laser" className='rounded-full'/>
+          <img src="src\assets\laser.gif" alt="laser" className='rounded-full mt-[30px]'/>
         </div>
 
         <div className='right-who flex-1 flex flex-col justify-center
-        gap-[20px]'>
-          <h1 className='text-[74px] font-[700]'>Think outside the square space</h1>
-          <section className='flex items-center gap-[20px]'>
-            <div className='w-[40px] h-[5px] bg-[#1eff00] rounded shadow-[0px_0px_30px_5px_rgba(0,255,89)]'></div>
-            <div className='text-[26px]'>Who we Are</div>
-          </section>
-          <div className='text-[24px] text-[#8e8e8e]'>A creative group of designers and developers with a passion for the arts.</div>
-          <button className='shadow-[0px_0px_30px_0px_rgba(0,255,89)] font-[700] w-[130px] h-[30px] text-[black] bg-[#1eff00] rounded-[5px] outline-none cursor-[pointer] hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] transition ease-in-out duration-500'>See our works</button>
+        gap-[50px]'>
+          <h1 className='text-[74px] font-[700]'>About Me</h1>
+          <div className='text-[24px] text-[#e8eaea] flex flex-col gap-[30px]'>
+            <div className='border-2 border-[#1eff00] px-8 py-4 rounded-[10px]'>I specialize in front-end web development, proficient in HTML5, CSS3, and TailwindCSS for crafting responsive and captivating interfaces.
+            </div>
+
+            <div className='border-2 border-[#67fffe] px-8 py-4 rounded-[10px]'>My JavaScript expertise enables me to create dynamic user experiences, coupled with mastery in ReactJS for building complex UIs.
+            </div>
+
+            <div className='border-2 border-[#ff0098] px-8 py-4 rounded-[10px]'>Currently, I'm learning backend development, using NodeJS, ExpressJS, and MongoDB to build robust server-side applications.
+            </div>
+          </div>
+          <button 
+          onClick={()=>scrollToSection('projects')}
+          className='shadow-[0px_0px_30px_0px_rgba(0,255,89)] font-[700] w-[130px] h-[30px] text-[black] bg-[#1eff00] rounded-[5px] outline-none cursor-[pointer] hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] transition ease-in-out duration-500'>See my work</button>
         </div>
       </section>
     </div>
