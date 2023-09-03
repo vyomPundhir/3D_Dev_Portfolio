b4904e194fab2dacc6aa0db2635fda9600faa536
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:12:58 +0530

    Card tech jsx file modified.

--- src/components/CardTech.jsx
+++ src/components/CardTech.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import {techStackNames} from '../constants';
 
-const CardTech = () => {
+const CardTech = ({techimage, techcontent, techcolor}) => {
+  
+    const boxShadowStyle = {
+      boxShadow: `0px 0px 30px 3px ${techcolor}`,
+    }
+
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src='' alt="tech" className="rounded-xl" />
+    <Tilt>
+      <div 
+      style={boxShadowStyle}
+      className='card w-96 bg-transparent pb-4 s'>
+        <figure className="px-5 pt-3 pb-1">
+          <img src={techimage} alt="tech" className="rounded-xl w-[150px] h-[150px]" />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">HTML5</h2>
+        <div className="items-center text-center">
+          <h2 className="text-[25px] font-[700]">{techcontent}</h2>
         </div>
       </div>
-    </div>
+    </Tilt>
   )
 }
 
