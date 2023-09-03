dad8d8c7e14b43327aae616686be0aa25e60e5fb
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:16:10 +0530

    Tech Stack jsx file modified.

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import CardTech from './CardTech'
-
+import {techStackNames} from '../constants';
 
 const TechStack = () => {
+  
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className='text-[white]'>Tech Stack</h1>
-      <div>
-        <CardTech/>
+    <div id='techstack' className='flex flex-col justify-center items-center gap-[50px]'>
+      <h1 className='text-[white] text-[70px] font-[700]'>Tech Stack</h1>
+      <div className='flex justify-center flex-wrap gap-[70px]'>
+      {
+        techStackNames.map((pj,index)=>(
+          <div key={index}>
+            <CardTech techimage={pj.image} techcontent={pj.content} techcolor={pj.color} />
+          </div>
+        ))
+      }
       </div>
     </div>
   )
 }
 
 export default TechStack
+
+
+
+
