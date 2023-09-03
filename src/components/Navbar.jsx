74ed668f7158e05f3d18affa57639e0798dba4e6
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:15:03 +0530

    Navbar jsx file modified.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,60 @@
 import React from 'react'
 
 const Navbar = () => {
+const scrollToSection = (id) => {
+  const element = document.getElementById(id);
+  element.scrollIntoView({ behavior: 'smooth' });
+};
+
   return (
-    <div className='flex justify-center'>
+    <div id='navbar' className='flex justify-center'>
 
       <div className='w-[1400px] flex justify-between items-center '>
 
         <section className='flex items-center gap-[40px]'>
 
-          <img src="src\assets\Logo.jpg" alt="logo" className='w-[130px] cursor-pointer'/>
+          <a href="/"><img src="src\assets\Logo.jpg" alt="logo" className='w-[130px] cursor-pointer'/></a>
 
           <section className='flex gap-[20px] cursor-pointer text-[#d3d1d1] font-[500]'>
 
-            <button className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[55px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Home</button>
+            <button 
+            onClick={()=>scrollToSection('about')}
+            className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[100px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>About</button>
 
-            <button className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,255,252)] w-[55px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Studio</button>
+            <button 
+            onClick={()=> scrollToSection('techstack')}
+            className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,255,252)] w-[100px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Tech Stack</button>
 
-            <button className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[55px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Works</button>
+            <button 
+            onClick={()=>scrollToSection('projects')}
+            className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[100px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Projects</button>
 
-            <button className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[65px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Contact</button>
+            <button 
+            onClick={()=>scrollToSection('blogs')}
+            className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[100px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Blogs</button>
+
+            <button 
+            onClick={()=>scrollToSection('contactus')}
+            className=' hover:text-[black] hover:bg-[white] hover:font-[500] hover:shadow-[0px_0px_30px_0px_rgba(252,252,252)] w-[100px] text-center rounded transition ease-in-out duration-500 brightness-75 hover:brightness-100'>Contact Us</button>
 
           </section>
 
         </section>
         
         <section className='flex gap-[25px]'>
-          <img src="src\assets\search.png" alt="search" className="w-[20px] h-[20px] cursor-pointer"/>
-          <button className='shadow-[0px_0px_30px_0px_rgba(0,255,89)] mt-[-5px] font-[700] w-[100px] h-[30px] text-[black] bg-[#1eff00] rounded-[5px] outline-none cursor-[pointer] hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] transition ease-in-out duration-500'>Hire Now</button>
+          
+          <button className='shadow-[0px_0px_30px_0px_rgba(0,255,89)] mt-[-5px] font-[700] w-auto px-5  h-auto py-1.5 text-md text-[black] bg-[#1eff00] rounded-[5px] outline-none cursor-[pointer] hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] transition ease-in-out duration-500'>
+            <a href="">Download Resume</a>
+          </button>
         </section>
+
+        
       </div>
+        <button
+          onClick={()=> scrollToSection('navbar')}
+          className='w-16 h-16 fixed z-10 bottom-12 right-12'>
+            <img src="src\assets\upArrow.png" alt="up arrow" />
+        </button>
     </div>
   )
 }
