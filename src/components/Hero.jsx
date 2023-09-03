1947e73c7b61f6f5597860b7466345b78e32f7ea
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-03 22:14:29 +0530

    Hero jsx file modified.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,23 +8,22 @@ const Hero = () => {
   return (
     <div className='m-0 p-0 box-border h-screen text-white snap-center flex justify-between items-center flex-col'>
       <Navbar/>
-      <section className='text-[white] min-h-screen snap-center w-[1400px]
-      flex justify-between'>
+      <section className='text-[white] min-h-screen snap-center w-full flex justify-between'>
 
         <div className='left basis-2/5 flex flex-col justify-center
-        gap-[20px]'>
+        gap-[40px]'>
 
-          <h1 className='text-[74px] font-[700]'>Think. Make. Solve.</h1>
+          <h1 className='text-[80px] font-[800]'>Hi, I'm <span className='text-[#1eff00]'>Vyom</span></h1>
 
           <section className='flex items-center gap-[20px]'>
 
             <div className='w-[40px] h-[5px] bg-[#1eff00] rounded shadow-[0px_0px_30px_5px_rgba(0,255,89)]'></div>
 
-            <div className='text-[26px]'>What we Do</div>
+            <div className='text-[26px]'>
+            I develop User Interfaces, APIs & Web Applications.
+            </div>
 
           </section>
-
-          <div className='text-[24px] text-[#8e8e8e]'>We enjoy creating delightful, human-centered digital experiences.</div>
 
           <button className='shadow-[0px_0px_30px_0px_rgba(0,255,89)] font-[700] w-[100px] h-[30px] text-[black] bg-[#1eff00] rounded-[5px] outline-none cursor-[pointer] hover:shadow-[0px_0px_30px_5px_rgba(0,255,89)] transition ease-in-out duration-500'>Learn more</button>
 
